4d8c54762c99b0b8bcac3af8cf9d4d6251b8f9d2
Phil Wang <lucidrains@gmail.com>
2021-12-14 18:01:15 -0800

    complete migration code for transformer feedforwards, but make sure to document that names need to be added to sonnet Linear instantiations in deepmind code

--- scripts/tf_to_torch.py
+++ scripts/tf_to_torch.py
@@ -19,20 +19,23 @@ def copy_attn_pool(mod, vars, path):
   attn_pool_proj = vars[path]
   mod.to_attn_logits.data.copy_(attn_pool_proj)
 
-def copy_linear(mod, vars, path, has_bias = True):
+def copy_linear(mod, vars, path):
   weight = vars[f'{path}w:0']
+  bias = vars[f'{path}b:0']
   mod.weight.data.copy_(rearrange(weight, 'i o -> o i'))
+  mod.bias.data.copy_(bias)
 
-  if has_bias:
-    bias = vars[f'{path}b:0']
-    mod.bias.data.copy_(bias)
+def copy_ln(mod, vars, path):
+  weight = vars[f'{path}scale:0']
+  bias = vars[f'{path}offset:0']
+  mod.weight.data.copy_(weight)
+  mod.bias.data.copy_(bias)
+
+def copy_attn(mod, var_path):
+  pass
 
 def get_tf_vars(tf_model):
   return {v.name: (torch.from_numpy(v.numpy()) if isinstance(v.numpy(), np.ndarray) else None) for v in tf_model.variables}
-
-def copy_attn(mod, var_path):
-  # todo
-  pass
 
 def copy_tf_to_pytorch(tf_model, pytorch_model):
   tf_vars = get_tf_vars(tf_model)
@@ -65,6 +68,22 @@ def copy_tf_to_pytorch(tf_model, pytorch_model):
     copy_conv(tower_point_conv, tf_vars, point_conv_path)
     copy_attn_pool(tower_attn_pool, tf_vars, attn_pool_path)
 
+  for ind, transformer_block in enumerate(pytorch_model.transformer):
+    ff_ln_path = f'enformer/trunk/transformer/transformer/transformer_block_{ind}/transformer_block_{ind}/mlp/mlp/layer_norm/'
+
+    # https://github.com/deepmind/deepmind-research/blob/master/enformer/enformer.py#L119
+    # needs to be edited to snt.Linear(channels * 2, name = 'project_in') and snt.Linear(channels, name = 'project_out') or variables are not accessible
+    ff_linear1_path = f'enformer/trunk/transformer/transformer/transformer_block_{ind}/transformer_block_{ind}/mlp/mlp/project_in/'
+    ff_linear2_path = f'enformer/trunk/transformer/transformer/transformer_block_{ind}/transformer_block_{ind}/mlp/mlp/project_out/'
+
+    ff = transformer_block[-1]
+    ff_ln = ff.fn[0]
+    ff_linear1 = ff.fn[1]
+    ff_linear2 = ff.fn[4]
+
+    copy_ln(ff_ln, tf_vars, ff_ln_path)
+    copy_linear(ff_linear1, tf_vars, ff_linear1_path)
+    copy_linear(ff_linear2, tf_vars, ff_linear2_path)
 
   final_bn = pytorch_model.final_pointwise[1][0]
   final_conv = pytorch_model.final_pointwise[1][2]
@@ -79,3 +98,5 @@ def copy_tf_to_pytorch(tf_model, pytorch_model):
   copy_linear(mouse_linear, tf_vars, 'enformer/heads/head_mouse/head_mouse/linear/')
 
   print('success')
+
+# copy_tf_to_pytorch(tf_model, pytorch_model)
