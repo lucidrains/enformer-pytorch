98e36020f999a50ee36c1aca184e51caabeb2bfc
Phil Wang <lucidrains@gmail.com>
2022-01-06 09:34:15 -0800

    prep for accepting sequence in string form, with period "." as padding

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'enformer-pytorch',
   packages = find_packages(exclude=[]),
   include_package_data = True,
-  version = '0.1.26',
+  version = '0.1.28',
   license='MIT',
   description = 'Enformer - Pytorch',
   author = 'Phil Wang',
