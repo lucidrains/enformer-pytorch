acc085d68252e21f0e0ffd923d0d9bfd140b951b
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-02-21 12:02:26 +0100

    Update README and remove legacy files

--- enformer_pytorch/demo.py
+++ enformer_pytorch/demo.py
@@ -2,18 +2,21 @@ from enformer_pytorch import EnformerConfig, Enformer, load_pretrained_model
 
 # load a model as follows
 
-model = Enformer.from_pretrained("nielsr/enformer-preview")
+# model = Enformer.from_pretrained("nielsr/enformer-preview")
+
+# for name, param in model.named_parameters():
+#     print(name, param.shape)
 
 # the code below was used to upload model weights to the hub
 
-# model_name = 'corr_coef_obj'
+model_name = '191k_corr_coef_obj'
 
-# config = EnformerConfig()
+config = EnformerConfig()
 
-# model = Enformer(config)
+model = Enformer(config)
 
-# # load pretrained weights
-# model = load_pretrained_model(model_name)
+# load pretrained weights
+model = load_pretrained_model(model_name)
 
-# # push to hub
-# model.push_to_hub(f"enformer-{model_name}", organization="nielsr", commit_message="Add model", use_temp_dir=True)
+# push to hub
+model.push_to_hub(f"enformer-{model_name}", organization="nielsr", commit_message="Add model", use_temp_dir=True)
