a6857e3ca1998dfa93ab2f7d9d71a3bce8c9d4bd
Phil Wang <lucidrains@gmail.com>
2022-02-23 13:11:43 -0800

    add a validation sample and some code for testing pretrained model

---
+++ test_pretrained.py
@@ -0,0 +1,19 @@
+import torch
+from enformer_pytorch import Enformer
+
+enformer = Enformer.from_pretrained('EleutherAI/enformer-preview').cuda()
+enformer.eval()
+
+data = torch.load('./data/test-sample.pt')
+seq, target = data['sequence'].cuda(), data['target'].cuda()
+
+with torch.no_grad():
+    corr_coef = enformer(
+        seq,
+        target = target,
+        return_corr_coef = True,
+        head = 'human'
+    )
+
+print(corr_coef)
+assert corr_coef > 0.1
