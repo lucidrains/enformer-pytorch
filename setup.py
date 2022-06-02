f5d36ddb90f82c5e28538d91bd3542cf2f9a99fb
Phil Wang <lucidrains@gmail.com>
2022-06-02 16:15:23 -0700

    0.5.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'enformer-pytorch',
   packages = find_packages(exclude=[]),
   include_package_data = True,
-  version = '0.5.1',
+  version = '0.5.2',
   license='MIT',
   description = 'Enformer - Pytorch',
   author = 'Phil Wang',
