e6526e3d7ba08de49f452bbc5f467834640046bc
Phil Wang <lucidrains@gmail.com>
2023-02-20 19:41:38 -0800

    allow for activation in finetuning head to be customizable, addressing https://github.com/lucidrains/enformer-pytorch/pull/23

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'enformer-pytorch',
   packages = find_packages(exclude=[]),
   include_package_data = True,
-  version = '0.6.4',
+  version = '0.7.1',
   license='MIT',
   description = 'Enformer - Pytorch',
   author = 'Phil Wang',
