adopt the fix proposed by @johahi for fixing the small numerical discrepancy in the pretrained model between tensorflow and pytorch https://github.com/lucidrains/enformer-pytorch/issues/31

test_pretrained.py
@@ -1,7 +1,7 @@
 import torch
-from enformer_pytorch import Enformer
+from enformer_pytorch import from_pretrained
 
-enformer = Enformer.from_pretrained('EleutherAI/enformer-official-rough').cuda()
+enformer = from_pretrained('EleutherAI/enformer-official-rough').cuda()
 enformer.eval()
 
 data = torch.load('./data/test-sample.pt')