d4ae222405f40a88036930811e5ba686b6ee9b64
lucidrains <lucidrains@gmail.com>
2024-10-07 10:36:38 -0700

    0.8.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'enformer-pytorch',
   packages = find_packages(exclude=[]),
   include_package_data = True,
-  version = '0.8.8',
+  version = '0.8.9',
   license='MIT',
   description = 'Enformer - Pytorch',
   author = 'Phil Wang',
